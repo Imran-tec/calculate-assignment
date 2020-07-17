c13628cb466db4e8a4cc83f1d728ac5b8f7ce778
Imran-tec <imranhossainbd508@gmail.com>
2020-07-17 23:44:41 +0600

    all-done

--- assignment.js
+++ assignment.js
@@ -15,7 +15,7 @@ console.log(result);
 
                         
                              //   woodCalculator
-                             
+
 
 function woodCalculator(chair, table, bed){
     var total = (chair * 1) + (table * 3) + (bed * 5);
@@ -32,28 +32,49 @@ function woodCalculator(chair, table, bed){
 
 
                                  // brickCalculator
-    
+  
+
+
+
+
+                                 
  function brickCalculator(floor){
-     if(floor < 0){
-         return 'digit is invalid';
-     }
-
-   else if (floor <= 10){
-        var floor = floor * 15000;
-    }
-
-    else if(floor <= 20){
-    var floor = floor * 12000;
-    }
-   
-   else{
-    var floor = floor * 10000;
+    var floor1;
+    var floor2;
+    var floor3;
+    var floor4;
+    
+   if (floor >=1 && floor<11 ){
+       var floor1 = floor * 15000;
+       return floor1;
    }
-      return floor;
- }
 
- var result = brickCalculator();
- console.log(result);
+   else if(floor>10  && floor<=20){
+     floor1 = floor - 10;
+     floor2 = floor1 * 12000+150000;
+ 
+    
+   return  floor2;
+
+   }
+  
+  else if (floor>20){
+
+   var floor3 = floor-20;
+   floor4 = floor3 * 10000 + 270000;
+  
+
+  return floor4;
+   
+  }
+  else{
+      return 'invalid digit'
+  }
+    
+}
+
+result = brickCalculator();
+console.log(result );
 
 
 
